fcc1bee69938a9424fe74cf1bec26f068765484f
canicemichael <canicemichael@gmail.com>
2023-02-28 21:30:51 +0100

    chore: change implementation of google-auth due to old method is deprecated

--- auth-server.js
+++ auth-server.js
@@ -1,22 +1,52 @@
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const { google } = require("googleapis");
+const client_secret = require("./client_secret.json");
+let token;
 
-const PORT = 5000;
-
-// midlleware
-app.set("view engine", "ejs");
 app.use(express.json());
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+const oauth2Client = new google.auth.OAuth2(
+  client_secret.web.client_id,
+  client_secret.web.client_secret,
+  client_secret.web.redirect_uris[0]
+);
+
+const scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+];
+
+const authorizationUrl = oauth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: "https://www.googleapis.com/auth/userinfo.profile",
+  include_granted_scopes: true,
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get("/test", async (req, res) => {
+  // This will provide an object with the access_token and refresh_token.
+  // Save these somewhere safe so they can be used at a later time.
+
+  res.send("This route is just for testing");
 });
 
-app.listen(PORT, () => {
-  console.log("server started at port 5000");
+app.get("/login", (_, res) => {
+  // Example on redirecting user to Google's OAuth 2.0 server.
+  // await res.writeHead(301, { Location: authorizationUrl });
+  // await res.send(authorizationUrl);
+  res.redirect(authorizationUrl);
+});
+
+app.get("/", async (req, res) => {
+  const code = req.query.code;
+  const { tokens } = await oauth2Client.getToken(code);
+  token = tokens.access_token
+  oauth2Client.setCredentials(tokens);
+  console.log(tokens);
+  res.send("Successfully authenticated access_token is " + token);
+});
+
+app.listen(5000, () => {
+  console.log("Server listening at port 5000");
 });
