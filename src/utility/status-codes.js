2fc99b889e7eb4284921b568780dd7ea44e01cf0
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-02-28 21:44:59 +0100

    fix: convert es6 export to module exports

--- src/utility/status-codes.js
+++ src/utility/status-codes.js
@@ -1,7 +1,7 @@
-export const BAD_REQUEST = 400;
-export const OK = 200;
-export const NOT_FOUND = 404;
-export const INTERNAL_SERVER_ERROR = 500;
-export const SERVICE_UNAVAILABLE = 503;
-export const PERMANENT_REDIRECT = 301;
-export const TEMPORARY_REDIRECT = 301;
+exports.BAD_REQUEST = 400;
+exports.OK = 200;
+exports.NOT_FOUND = 404;
+exports.INTERNAL_SERVER_ERROR = 500;
+exports.SERVICE_UNAVAILABLE = 503;
+exports.PERMANENT_REDIRECT = 301;
+exports.TEMPORARY_REDIRECT = 301;
