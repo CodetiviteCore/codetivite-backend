c278a415ea9df19cf27d3b1e41633948880cf2f1
Emmanuel Ngene <emmanuelchinazangene@naurix.com>
2023-03-16 20:28:42 +0100

    feat: added authorization middleware

---
+++ src/middlewares/authorize.js
@@ -0,0 +1,22 @@
+const { UNAUTHORIZED } = require("../utility/status-codes");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+module.exports = function (req, _, next) {
+  const splittedAuthHeader = req.headers.authorization?.split(" ");
+
+  if (!splittedAuthHeader) {
+    return res.status(UNAUTHORIZED).send("No authorization token specified in headers");
+  }
+
+  if (splittedAuthHeader.length != 2) {
+    return res.status(UNAUTHORIZED).send("Invalid authorization header specified.\nIt must be a bearer auth token");
+  }
+
+  try {
+    req.user = jwt.verify(token, JWT_SECRET_KEY);
+  } catch (error) {
+    res.status(UNAUTHORIZED).send("Invalid authorization token");
+  }
+
+  next();
+};
